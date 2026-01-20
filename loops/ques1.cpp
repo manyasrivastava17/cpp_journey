29bc631a1cabc1f2c11f7751c12e7cf6d1848eec
manyasrivastava17 <manyasrivastava0730@gmail.com>
2026-01-20 23:44:03 +0530

    Added loops programs

---
+++ loops/ques1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+ using namespace std;
+
+int main()
+{
+  int n;
+  cout<<"Enter a number:";
+  cin>>n;
+
+  for(int i=1;i<=n;i++){
+    cout<<" Hellow World "<<endl;
+  }
+  return 0;
+}
