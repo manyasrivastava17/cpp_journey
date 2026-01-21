dba01a4b5dfdad451f63d6ead1b0dc35f55ccff3
manyasrivastava17 <manyasrivastava0730@gmail.com>
2026-01-21 23:50:39 +0530

    Added question folder programs

---
+++ question/ques2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int n;
+  cout<<"enter the marks:";
+  cin>>n;
+
+  if(n>=33){
+    cout<<"pass";
+  }
+  else{
+    cout<<"fail";
+  }
+  return 0;
+}
---
+++ question/ques3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int radius;
+  cout<<"enter radius of circle: ";
+  cin>>radius;
+
+  cout<<3.14*radius*radius;
+  return 0;
+}
